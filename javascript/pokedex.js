7ccf6cc17fd768dfe1f3b0876c000c935df3a129
Daniel Ramirez <contact@dann.dev>
2022-04-07 12:27:56 -0500

    fix: navigation buttons in ipad

--- javascript/pokedex.js
+++ javascript/pokedex.js
@@ -7,17 +7,43 @@ const $image = document.querySelector('#image');
 const $textContent = document.querySelector('#textContent');
 const $screen = document.querySelector('#screen');
 
+const $navigationRight = document.querySelector('#navigationRight');
+const $navigationLeft = document.querySelector('#navigationLeft');
+const $navigationUp = document.querySelector('#navigationUp');
+const $navigationDown = document.querySelector('#navigationDown');
+
 const $name = document.querySelector('#name');
 const $number = document.querySelector('#number');
 
 $name.addEventListener('focus',clearInput);
 $number.addEventListener('focus',clearInput);
 
+$navigationRight.addEventListener('click', nextPokemon);
+$navigationLeft.addEventListener('click', prevPokemon);
+$navigationUp.addEventListener('click', nextImage);
+$navigationDown.addEventListener('click', prevImage);
+
+function nextPokemon(){
+    console.log("Boton presionado1");
+};
+
+function prevPokemon(){
+    console.log("Boton presionado2");
+};
+
+function nextImage(){
+    console.log("Boton presionado3");
+};
+
+function prevImage(){
+    console.log("Boton presionado4");
+};
+
+
 function clearInput(event){
         $name.value = '';
         $number.value = '';
 }
-
 
 async function findPokemon(id) {
     const pokemon = await getPokemon(id);
