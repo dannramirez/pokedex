38fe913b4f820d0f1bcdfac9d1a28a2a7903d8a6
Daniel Ramirez <contact@dann.dev>
2022-04-07 17:15:53 -0500

    Feat: keep open cover after search a pokemon

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@ import {
     setPokemon
 } from "./pokedex.js";
 
+const $pokedex = document.querySelector('#pokedex');
 const $form1 = document.querySelector('#form1');
 const $form2 = document.querySelector('#form2');
 
@@ -23,6 +24,7 @@ let infoActualPokemon = null;
 
 async function handleSubmit(event) {
     event.preventDefault();
+    $pokedex.classList.add('is-open');
     const form1 = new FormData($form1);
     const form2 = new FormData($form2);
     const number = form1.get('number');
