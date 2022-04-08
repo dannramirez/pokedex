8e1d2fb3bf7fcaab95809e04bb3780b713d92c92
Daniel Ramirez <contact@dann.dev>
2022-04-08 16:37:52 -0500

    fix:Pokemon not found

--- javascript/api.js
+++ javascript/api.js
@@ -6,7 +6,7 @@ async function getPokemon(id) {
         if (response.status !== 200) {
             return await {
                 sprites: {
-                    front_default: "/pokedex/images/pokeball.svg"
+                    front_default: "/images/pokeball.svg"
                 },
                 name: "No existe",
                 id: 0
